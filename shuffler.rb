1adf98fb1110e9a451fa55561a043d2ea942d92b
Rudzainy_Rahman <rudzainy@gmail.com>
2016-05-11 03:32:16 -0400

    Add shuffler.rb

---
+++ shuffler.rb
@@ -0,0 +1,12 @@
+def shuffle(array)
+  # Implement the shuffle method
+end
+
+
+# Driver code:
+sorted_array = (1..10).to_a
+
+# This should print a different sequence of numbers each time
+p shuffle(sorted_array)
+p shuffle(sorted_array)
+p shuffle(sorted_array)
